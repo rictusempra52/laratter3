4db8dd795555822382db7bd649666af4fdd937bf
rictusempra52 <rictusempra52@gmail.com>
2025-02-11 19:56:26 +0900

    tweet作成処理のコメントを追加

--- resources/views/tweets/create.blade.php
+++ resources/views/tweets/create.blade.php
@@ -1,3 +1,1 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
+{{-- tweet作成処理 --}}
