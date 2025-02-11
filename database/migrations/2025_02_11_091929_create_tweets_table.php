b94c3d4ed60b16fe053f140769a8e928ad9d8069
rictusempra52 <rictusempra52@gmail.com>
2025-02-11 18:39:48 +0900

    ツイートテーブルにユーザーIDとツイート内容のカラムを追加

--- database/migrations/2025_02_11_091929_create_tweets_table.php
+++ database/migrations/2025_02_11_091929_create_tweets_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tweets', function (Blueprint $table) {
             $table->id();
+            // ユーザーID
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            // ツイート内容
+            $table->string('tweet');
             $table->timestamps();
         });
     }
